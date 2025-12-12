1b0940ab95f2d2900b202c36577dea84c0e64ff8
bhattyuvraj22 <bhatt.yuvraj22@gmail.com>
2025-12-12 18:41:24 +0530

    fix ui and max. ui complated

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const audio = document.getElementById("player");
-const rawUrl = "https://bhattyuvraj22.github.io/web-music-app/son";
+const player = document.getElementById("player");
 
-// fetch as blob then play
-async function loadAndPlay() {
-  try {
-    const res = await fetch(rawUrl);
-    if (!res.ok) throw new Error("Failed to fetch: " + res.status);
-    const blob = await res.blob();
-    const objectUrl = URL.createObjectURL(blob);
-    audio.src = objectUrl;
-    // audio.play(); // requires user gesture to autoplay on most browsers
-  } catch (err) {
-    console.error(err);
-  }
-}
+document.addEventListener("click", e => {
+    if (e.target.classList.contains("playbtn")) {
+        const card = e.target.closest(".card2");
+        const audioSrc = card.dataset.audio;
 
-loadAndPlay();
+        player.src = audioSrc;
+        player.play();
+    }
+});
+
+
+
+
